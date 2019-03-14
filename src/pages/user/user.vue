e16c4de719fcfe6474c894a5cb2ae4e323ef707d
wpchen66 <cwp956096332@outlook.com>
2019-03-14 21:15:55 +0800

    user

--- src/pages/user/user.vue
+++ src/pages/user/user.vue
@@ -2,6 +2,12 @@
     <div id="user">
         <div class="u_header">
             <i class="u_arrow_icon"></i>
+            个人中心
+        </div>
+        <div class="u_avatar_wrap">
+            <div class="u_avatar">
+                <img src="../../assets/images/index/avatar.jpg" alt="">
+            </div>
         </div>
     </div>
 </template>
@@ -16,10 +22,35 @@ export default {
     @media (-webkit-min-device-pixel-ratio: 3),(min-device-pixel-ratio: 3) {
         background-image: url("../../assets/images/user/@{url}@3x.png");
     }
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
 }
 .u_header{
+    text-align: center;
+    font-size: 32px;
+    height: 90px;
+    line-height: 90px;
+    width: 750px;
+    background-color:#fff; 
+    position: relative;
     .u_arrow_icon{
-        .images('l_arrow')
+        .images('l_arrow');
+        width: 16px;
+        height: 30px;
+        display: block;
+        position: absolute;
+        top:30px;
+        left:35px;
+    }
+}
+.u_avatar_wrap{
+    height: 178px;
+    width: 738px;
+    .u_avatar{
+        width: 118px;
+        height: 118px;
+        border-radius: 59px;
+        overflow: hidden;
     }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 Vue.use(Router)
+
+const App = (resolve) => {
+  import('../pages/index/App').then(module =>{
+    resolve(module)
+  })
+} 
+const User = (resolve) => {
+  import('../pages/user/user').then(module =>{
+    resolve(module)
+  })
+} 
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      component: User
+    }
   ]
 })
