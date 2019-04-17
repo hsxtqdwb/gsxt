00d3edb7507b8cb33885c7ea897b79cba01916d1
chenweiping <chenweiping@126.com>
2019-04-17 17:54:09 +0800

    index

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from 'pages/wrap/index'
 import router from './router'
 import FastClick from 'fastclick'
+import axios from 'axios'
 import wx from 'weixin-js-sdk'
-
 import 'lib-flexible'
 
 import './assets/css/reset.css'
+const appid = 'wx88a319f29c9a7c51'
+const redirect_uri = 'http://192.168.0.101'
 Vue.config.productionTip = false
 if ('addEventListener' in document) {
   document.addEventListener('DOMContentLoaded', function() {
@@ -16,6 +18,10 @@ if ('addEventListener' in document) {
   }, false);
 }
 
+
+axios.get(`/connect/oauth2/authorize?appid=wx88a319f29c9a7c51&redirect_url=http://192.168.0.101&response_type=code&scope=snsapi_userinfo#wechat_redirect`).then(res =>{
+  alter(res)
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
