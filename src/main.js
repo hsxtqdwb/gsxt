c3a05bc1c42544961f5af55909c141b39d1cb0f8
chenweiping <chenweiping@126.com>
2019-05-21 10:58:36 +0800

    hah

--- src/main.js
+++ src/main.js
@@ -11,49 +11,56 @@ import 'lib-flexible'
 
 // Vue.prototype.$http = new Http()
 import './assets/css/reset.css'
-import { setItem, getItem } from './utils';
+import {
+  setItem,
+  getItem
+} from './utils';
 Vue.config.productionTip = false;
 Vue.prototype.http = new Http()
 
-FastClick.prototype.focus = function(targetElement) {
+FastClick.prototype.focus = function (targetElement) {
   var length;
   var deviceIsWindowsPhone = navigator.userAgent.indexOf("Windows Phone") >= 0;
   var deviceIsIOS = /iP(ad|hone|od)/.test(navigator.userAgent) && !deviceIsWindowsPhone;
   // Issue #160: on iOS 7, some input elements (e.g. date datetime month) throw a vague TypeError on setSelectionRange. These elements don't have an integer value for the selectionStart and selectionEnd properties, but unfortunately that can't be used for detection because accessing the properties also throws a TypeError. Just check the type instead. Filed as Apple bug #15122724.
   if (deviceIsIOS && targetElement.setSelectionRange && targetElement.type.indexOf('date') !== 0 && targetElement.type !== 'time' && targetElement.type !== 'month') {
-  length = targetElement.value.length;
-  targetElement.focus();
-  targetElement.setSelectionRange(length, length);
+    length = targetElement.value.length;
+    targetElement.focus();
+    targetElement.setSelectionRange(length, length);
   } else {
-  targetElement.focus();
+    targetElement.focus();
   }
-  };
+};
 
 if ('addEventListener' in document) {
-  document.addEventListener('DOMContentLoaded', function() {
-      FastClick.attach(document.body);
+  document.addEventListener('DOMContentLoaded', function () {
+    FastClick.attach(document.body);
   }, false);
 }
 
-router.beforeEach((to,from,next)=>{
-  
-  const OPEN_ID = getItem('OPEN_ID')
-  if(to.query.code&&!OPEN_ID){
-    const CODE = to.query.code
-    new Http().get(`/sw/metadata/DataSerController/getdata.do?servicecode=10007&grantcode=88888888`,{
-      CODE
-    })
-    .then(res=>{
-      if(res.invokeResultCode==='000'&&!res.result.errcode){
-        const OPEN_ID = res.result.openid
-        const USER_INFO = res.result
-        setItem('OPEN_ID',OPEN_ID)
-        setItem('USER_INFO',USER_INFO)
-        next(`/`)
-      }
-    })
-  }else if(!OPEN_ID){
-    next('/author')
+router.beforeEach((to, from, next) => {
+  console.log(to)
+  if (to.path!=='/author') {
+    const OPEN_ID = getItem('OPEN_ID')
+    if (to.query.code && !OPEN_ID) {
+      const CODE = to.query.code
+      new Http().get(`/sw/metadata/DataSerController/getdata.do?servicecode=10007&grantcode=88888888`, {
+          CODE
+        })
+        .then(res => {
+          if (res.invokeResultCode === '000' && !res.result.errcode) {
+            const OPEN_ID = res.result.openid
+            const USER_INFO = res.result
+            setItem('OPEN_ID', OPEN_ID)
+            setItem('USER_INFO', USER_INFO)
+            next(`/`)
+          }
+        })
+    } else if (!OPEN_ID) {
+      next('/author')
+    } else {
+      next()
+    }
   }else{
     next()
   }
@@ -67,6 +74,8 @@ router.beforeEach((to,from,next)=>{
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
